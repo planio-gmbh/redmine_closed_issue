4f400ccaf33eb21877fbfa8a0016ff003ecd2d3b
Holger Just <holger@plan.io>
2013-07-01 10:22:14 +0200

    Remove useless SQL query which messes with in-flight issue state.

--- lib/redmine_closed_issue/hooks.rb
+++ lib/redmine_closed_issue/hooks.rb
@@ -7,14 +7,11 @@ module RedmineClosedIssue
     end
 
     def controller_issues_edit_before_save(context={})
-      if context[:issue] then
-        @issue = Issue.find context[:issue].id
-        unless @issue.status.id == context[:issue].status.id then # CHANGED STATUS
-          if context[:issue].status.is_closed?
-            context[:issue].closed_date = Time.now
-          else
-            context[:issue].closed_date = nil if(context[:issue].closed_date)
-          end
+      if context[:issue] && context[:issue].status_id_changed?
+        if context[:issue].status.is_closed?
+          context[:issue].closed_date = Time.now
+        else
+          context[:issue].closed_date = nil if(context[:issue].closed_date)
         end
       end
     end
